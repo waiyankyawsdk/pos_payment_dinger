3a5ec4e7969fe80fd0dbc76e025495b077fcc0b5
tkyawwai678@gmail.com <tkyawwai678@gmail.com>
2025-05-12 10:32:46 +0630

    [FIX] Module name changed.

--- static/src/app/overrides/models/models.js
+++ static/src/app/overrides/models/models.js
@@ -1,4 +1,4 @@
 import { register_payment_method } from "@point_of_sale/app/store/pos_store";
-import { PaymentDinger } from "@pos_test/app/payment_dinger";
+import { PaymentDinger } from "@pos_payment_dinger/app/payment_dinger";
 
 register_payment_method("dinger", PaymentDinger);
